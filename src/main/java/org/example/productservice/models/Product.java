27cbe78fc6807e8fec127d190c59f6be4100548b
Aditya <adityazoot@gmail.com>
2024-05-04 18:31:18 +0530

    Add test method

--- src/main/java/org/example/productservice/models/Product.java
+++ src/main/java/org/example/productservice/models/Product.java
@@ -3,11 +3,14 @@ package org.example.productservice.models;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Getter
 @Setter
@@ -20,6 +23,7 @@ public class Product extends BaseModel{
     private String price;
     private String description;
     private String image;
+
 
     @ManyToOne(cascade = CascadeType.PERSIST)
     private Category category;
--- src/test/java/org/example/productservice/ProductserviceApplicationTests.java
+++ src/test/java/org/example/productservice/ProductserviceApplicationTests.java
@@ -1,13 +1,35 @@
 package org.example.productservice;
 
+import org.example.productservice.models.ProductTitleAndDesc;
+import org.example.productservice.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.logging.Logger;
 
 @SpringBootTest
 class ProductserviceApplicationTests {
 
+	Logger log = Logger.getLogger(ProductserviceApplicationTests.class.getName());
+	@Autowired
+	@Qualifier("SelfProductService")
+	ProductService productService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getProductTitleAndDesc()
+	{
+		Long id =102L;
+		ProductTitleAndDesc productTitleAndDesc = productService.getProductTitleandDescription(id);
+		String a = productTitleAndDesc.getTitle();
+		String b = productTitleAndDesc.getDescription();
+		log.info("Title is : "+a +"Description is : "+b);
+		return;
 	}
 
 }
